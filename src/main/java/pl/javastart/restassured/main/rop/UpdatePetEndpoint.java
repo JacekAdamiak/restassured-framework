e660fda01596b4cda05261eb9c42a5f4d0850db0
Jaca <jacek.adamiak@gazeta.pl>
2023-01-27 18:29:23 +0100

    Update pet test added

--- src/main/java/pl/javastart/restassured/main/rop/UpdatePetEndpoint.java
+++ src/main/java/pl/javastart/restassured/main/rop/UpdatePetEndpoint.java
@@ -1,5 +1,6 @@
 package pl.javastart.restassured.main.rop;
 
+import io.qameta.allure.Step;
 import org.apache.http.HttpStatus;
 import pl.javastart.restassured.main.pojo.pet.Pet;
 import pl.javastart.restassured.main.request.configuration.RequestConfigurationBuilder;
@@ -17,6 +18,7 @@ public class UpdatePetEndpoint extends BaseEndpoint<UpdatePetEndpoint, Pet>{
         return Pet.class;
     }
 
+    @Step("Update pet")
     @Override
     public UpdatePetEndpoint sendRequest() {
         response = given()
